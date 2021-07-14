c2f02729b0d3cf4015456c1c162eca90caa10353
shaddock <shaddock@live.com>
2021-07-14 11:20:51 -0500

    wip

---
+++ Blazor.Domain/Country.cs
@@ -0,0 +1,8 @@
+﻿namespace Blazor.Domain
+{
+    public class Country
+    {
+        public int CountryId { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ Blazor.Domain/MaritalStatus.cs
@@ -0,0 +1,9 @@
+﻿namespace Blazor.Domain
+{
+    public enum MaritalStatus
+    {
+        Married,
+        Single,
+        Other
+    }
+}
