b9decedc505de334650cb456906e5bf5af2db6dc
Sean Haddock <seanhaddock@live.com>
2021-07-16 11:35:13 -0500

    wip

---
+++ BlazorWebAssembly/Pages/EmployeeEdit.razor.cs
@@ -0,0 +1,26 @@
+﻿using Blazor.Domain;
+using BlazorWebAssembly.Services;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorWebAssembly.Pages
+{
+    public partial class EmployeeEdit : ComponentBase
+    {
+        [Inject]
+        public IEmployeeDataService EmployeeDataService { get; set; }
+
+        [Parameter]
+        public string EmployeeId { get; set; }
+
+        public Employee Employee { get; set; } = new Employee();
+
+        protected override async Task OnInitializedAsync()
+        {
+            Employee = await EmployeeDataService.GetEmployee(int.Parse(EmployeeId));
+        }
+    }
+}
